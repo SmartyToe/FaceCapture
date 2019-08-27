1cb511da57d06f0b1cf1b761320f47ab25169700
amir <amir.lahav@shutterfly.com>
2019-08-27 23:24:57 +0300

    Initial Commit

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by amir.lahav on 27/08/2019.
+//  Copyright © 2019 amir.lahav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
